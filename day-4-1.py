689754918d4531a6092d37d9a5437f628658538f
林煥培 <abc2464831@gmail.com>
2021-01-23 17:47:34 +0800

    day4

---
+++ day-4-1.py
@@ -0,0 +1,14 @@
+import random
+import My_module
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+random_float = random.random()
+print(random_float)
+
+# print(My_module.pi)
+# 若要用模組的話 後面必須用 .(函數名字))
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
